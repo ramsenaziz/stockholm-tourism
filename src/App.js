0e42b4042e32a8a84b8cb4b732c0f5fdcbf9b976
Ramsen Aziz <ramsen@onlineforce.net>
2018-04-15 08:08:48 +0200

    Finished up the mapping of locations, marker and searchfilter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from 'google-map-react'
+import Location from './components/Location'
 import Marker from './components/Marker'
 import DeleteButton from './components/DeleteButton'
 import SaveLocationForm from './components/SaveLocationForm';
@@ -14,66 +15,124 @@ class App extends Component {
         { 
           id: 1,
           name: "Stureplan",
-          lat: "59.339832", 
-          lng: "18.075802" 
+          lat: 59.339832, 
+          lng: 18.075802 
         },
         { 
           id: 2,
           name: "Kungsgatan",
-          lat: "59.334409", 
-          lng: "18.059047" 
+          lat: 59.334409, 
+          lng: 18.059047 
         },
         { 
           id: 3,
           name: "Medborgarplatsen",
-          lat: "59.315153", 
-          lng: "18.071664" 
+          lat: 59.315153, 
+          lng: 18.071664 
         },
       ],
-    }
+      allLocations: [
+        { 
+          id: 1,
+          name: "Stureplan",
+          lat: 59.339832, 
+          lng: 18.075802 
+        },
+        { 
+          id: 2,
+          name: "Kungsgatan",
+          lat: 59.334409, 
+          lng: 18.059047 
+        },
+        { 
+          id: 3,
+          name: "Medborgarplatsen",
+          lat: 59.315153, 
+          lng: 18.071664 
+        },
+      ],
+      selectedLocation: null,
+      search: ""
+    };
 
-    this.focusOnLocation = this.focusOnLocation.bind(this);
+    
+  }
+  
+  selectLocation = (location) => {
+    this.setState({
+      selectedLocation: location
+    });
   }
 
-  // handleClick(lat, lng) {
-  //    console.log(lat, lng);
-  //   };
-
+  handleSearch = (event) => {
+    this.setState({
+      search: event.target.value,
+      locations: this.state.allLocations.filter((location) => new RegExp(event.target.value, "i").exec(location.name))
+    });
+  }
 
   render() {
-    const center = {
+    let allLocations = this.state.locations;
+
+    let center = {
       lat: 59.32,
       lng: 18.06
     }
-    const locations = this.state.locations;
-    const listItems = locations.map((location) =>
-      <li key={location.id}><span className="location-name">{location.name}</span><DeleteButton /></li>
-    );
+
+    let zoom = 11;
+
+    if (this.state.selectedLocation) {
+      center = {
+        lat: this.state.selectedLocation.lat,
+        lng: this.state.selectedLocation.lng
+      }
+      zoom = 14;
+    }
 
     return (
       <div className="App">
         <div className="main">
-
           <div className="map-container">
             <GoogleMapReact
               center={center}
-              zoom={11}
-              onClick={this.handleClick}
+              zoom={zoom}
             >
             {this.state.locations.map((location) => {
-              return <Marker key={location.id} lat={location.lat} lng={location.lng} text={location.name} /> 
-          })}
-
+              return <Marker 
+                        key={location.id} 
+                        lat={location.lat} 
+                        lng={location.lng} 
+                        text={location.name} 
+                        selected={location === this.state.selectedLocation}
+                      /> 
+            })}
             </GoogleMapReact>
           </div>
 
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={this.state.search}
+              onChange={this.handleSearch} />
+          </div>
+          
           <div className="list-container">
             <h2>Saved Places</h2>
             <ol>
-              {listItems}
-            </ol>
+            {this.state.locations.map((location) => {
+              return <li key={location.id}>
+                        <Location 
+                          location={location} 
+                          selectLocation={this.selectLocation}
+                        />
+                        <DeleteButton />
+                      </li>
+            })}
+            </ol>      
           </div>
 
+          <SaveLocationForm />
         </div>
       </div>
     );
