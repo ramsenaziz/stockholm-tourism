adb80c0dd688baa7eb40c6408ad447a6a33b33ba
Ramsen Aziz <ramsen@onlineforce.net>
2018-04-19 20:41:02 +0200

    Fixed identing.

--- src/components/Location.js
+++ src/components/Location.js
@@ -10,8 +10,7 @@ class Location extends Component {
         return (
             <span 
                 className="location" 
-                onClick={this.handleClick}>
-                {name}
+                onClick={this.handleClick}>{name}
             </span>
         );
     }
