19766f5036f10032a92bc5abdc436017b5df665f
Ramsen Aziz <ramsen@onlineforce.net>
2018-04-26 01:50:54 +0200

    Added proptypes to component

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './Location.css'
 
 class Location extends Component {
@@ -14,6 +15,10 @@ class Location extends Component {
             </span>
         );
     }
+}
+
+Location.propTypes = {
+    onClick: PropTypes.func
 }
 
 export default Location
