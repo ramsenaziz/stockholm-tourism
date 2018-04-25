713607082be0937a46a71aa970d4a49e560e60c5
Ramsen Aziz <ramsen@onlineforce.net>
2018-04-26 01:55:08 +0200

    Moved config data for firebase from index.html to seperate file.

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyDt3AbxuYns89qCE47oRk4522XZkt-3YBU",
+    authDomain: "stockholm-touris-1523846957092.firebaseapp.com",
+    databaseURL: "https://stockholm-touris-1523846957092.firebaseio.com",
+    projectId: "stockholm-touris-1523846957092",
+    storageBucket: "stockholm-touris-1523846957092.appspot.com",
+    messagingSenderId: "885347929721"
+};
